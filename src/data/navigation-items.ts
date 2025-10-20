bd1805c10fe12e4ac40740225f7ffea209e6df02
Camilo <cj_rodelo@hotmail.com>
2025-10-19 21:56:32 -0500

    feat: new class to control audio files and updated on game-menu.ts

--- src/data/navigation-items.ts
+++ src/data/navigation-items.ts
@@ -2,15 +2,11 @@ import type { MenuOption } from '@/types/game-menu';
 
 export const navigationItems: MenuOption[] = [
   {
-    text: 'About Me',
-    url: '/about',
-  },
-  {
     text: 'Projects',
     url: '/projects',
   },
   {
-    text: 'Contact',
-    url: '/contact',
+    text: 'About',
+    url: '/about',
   },
 ];
