e33b927e029f00ae9fd949f3e2df5f34f7d64101
Camilo <cj_rodelo@hotmail.com>
2025-10-26 12:15:12 -0500

    feat: created a file to map info inside about

---
+++ src/data/aboutSectionInfo.ts
@@ -0,0 +1,24 @@
+export type aboutProject = {
+  title: string;
+  smallDescription: string;
+  mediaSrc: string;
+  link: string;
+  stack: string[];
+};
+
+export const featuredProjects: aboutProject[] = [
+  {
+    title: 'Rop ST Digitall',
+    smallDescription: 'Description of Project 1',
+    mediaSrc: '',
+    link: '',
+    stack: [''],
+  },
+  {
+    title: 'Project 2',
+    smallDescription: 'Description of Project 2',
+    mediaSrc: '',
+    link: '',
+    stack: [''],
+  },
+];
