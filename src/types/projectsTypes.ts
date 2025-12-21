929403415dda2f18b0e47e5538193178240881b4
4zin <cj_rodelo@hotmail.com>
2025-12-20 23:09:55 -0500

    feat: add CardDetail and ProjectsCard components for project display and details

---
+++ src/types/projectsTypes.ts
@@ -0,0 +1,19 @@
+export type smallDetails = {
+  title: string;
+  smallDescription: string;
+  projectLogo: ImageMetadata;
+  link: string;
+  stack: string[];
+  redirect: string;
+};
+
+export type Project = {
+  projectLogo: ImageMetadata;
+  title: string;
+  id: string;
+  description?: string;
+  techStack?: string[];
+  link: string;
+  year: string;
+  role: string;
+};
